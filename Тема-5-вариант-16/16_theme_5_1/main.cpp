177f560cd32b02339ea8e96644cd5754129c177b
ruslan <kairulla@github.com>
2020-10-26 14:50:13 +0600

    использовал препроцессорное средство #define

--- Тема-5-вариант-16/16_theme_5_1/main.cpp
+++ Тема-5-вариант-16/16_theme_5_1/main.cpp
@@ -3,33 +3,37 @@
 #include <math.h>
 #include <stdio.h>
 #include <string>
+#define writeln endl
+#define celoe int
+#define write cout
+#define read cin
 
 using namespace std;
 
 double amount(double a);
 double composition(double d, double m);
 
-int main() {
+celoe main() {
     double a = 0, d = 0, m = 0, y = 0;//Объявил переменные
-    cout << "Введите параметры D, M, A" << endl << "D = ";//Ввод данных
-    cin >> d;
-    cout << endl << "M = ";
-    cin >> m;
-    cout << endl << "A = ";
-    cin >> a;
-    cout << endl;
+    write << "Введите параметры D, M, A" << writeln << "D = ";//Ввод данных
+    read >> d;
+    write << writeln << "M = ";
+    read >> m;
+    write << writeln << "A = ";
+    read >> a;
+    write << writeln;
 ///////////////////////////////////////////////////////////////////////////////
     
     y = composition(d, m) / amount(a); //Решение уравнения
     
-    cout << "Решение Y" << endl << "Y = " << y << endl; //Вывод решения
+    write << "Решение Y" << writeln << "Y = " << y << writeln; //Вывод решения
     
     return 0;
 }
 ///////////////////////////////////////////////////////////////////////////////
 
 double composition(double d, double m) {//Вычисляет числитель
-    int k = 1;
+    celoe k = 1;
     double result_composition = 0, result_multiply = 1;
     while (k < 5) {
         result_multiply *= k + d;
@@ -41,7 +45,7 @@ double composition(double d, double m) {//Вычисляет числитель
 ///////////////////////////////////////////////////////////////////////////////
 
 double amount(double a) {//Высчитывает знаменатель
-    int result_sum = 0, i = 1;
+    celoe result_sum = 0, i = 1;
     double result_amount = 0;
     do {//Цикл вычисляет сумму
         result_sum += i + 5;
